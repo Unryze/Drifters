6cb1aed7f88af271dfe0bf57bad15d733bf45f44
Sandro Takaishvili <SandroTakayshvili@gmail.com>
2018-08-27 19:33:10 +0400

    File list updated.

--- !MakeCode.bat
+++ !MakeCode.bat
@@ -1,6 +1,6 @@
 copy /b Scripts\Systems\Global.j + Scripts\Systems\CCEngine.j + Scripts\Systems\EffectsEngine.j + Scripts\Systems\StopSpell.j + Scripts\Systems\DisplaceUnit.j + Scripts\Systems\LinearUnitMovement.j + Scripts\Systems\DrawDamage.j + Scripts\Systems\DamageEngine.j + ^
-Scripts\Systems\AoEDisplaceEngine.j + Scripts\Systems\AoEDamageEngine.j + Scripts\Systems\SpiralMovement.j + Scripts\Systems\InitDefaultData.j + Scripts\Systems\Misc.j + Scripts\Systems\EnteringUnit.j + Scripts\Systems\AI.j + ^
-Scripts\Systems\HeroPick.j + Scripts\Systems\HeroLevelUp.j + Scripts\Systems\Multiboard.j + Scripts\Systems\Revive.j + Scripts\Systems\UnitDamaged.j + Scripts\Systems\ItemAoEFire.j + Scripts\Systems\ItemCombinationData.j + Scripts\Systems\LeaveAndWinnerCheck.j + ^
+Scripts\Systems\AoEDisplaceEngine.j + Scripts\Systems\AoEDamageEngine.j + Scripts\Systems\ProjectileMovement.j + Scripts\Systems\SpiralMovement.j + Scripts\Systems\InitDefaultData.j + Scripts\Systems\Misc.j + Scripts\Systems\EnteringUnit.j + Scripts\Systems\AI.j + ^
+Scripts\Systems\HeroPick.j + Scripts\Systems\HeroLevelUp.j + Scripts\Systems\Multiboard.j + Scripts\Systems\Revive.j + Scripts\Systems\UnitDamaged.j + Scripts\Systems\ItemAoEFire.j + Scripts\Systems\ItemPickUp.j + Scripts\Systems\LeaveAndWinnerCheck.j + ^
 Scripts\Heroes\Nanaya.j + Scripts\Heroes\Toono.j + Scripts\Heroes\Ryougi.j + Scripts\Heroes\SaberAlter.j + Scripts\Heroes\SaberNero.j + Scripts\Heroes\Byakuya.j + Scripts\Heroes\Akame.j + Scripts\Heroes\Scathach.j + Scripts\Heroes\Akainu.j + ^
 Scripts\Heroes\Reinforce.j + Scripts\Heroes\Arcueid.j + Scripts\Heroes\SaberArtoria.j + Scripts\Heroes\RubyRouse.j + ^
 Scripts\Systems\TestCommands.j + Scripts\EndPart.j war3map.j
